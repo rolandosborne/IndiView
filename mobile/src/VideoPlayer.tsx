d09e1a4056cb1c071f9886ce944e4dbad9fb68bb
Roland Osborne <roland.osborne@gmail.com>
2021-09-03 14:26:59 -0700

    apply default mute state

--- mobile/src/VideoPlayer.tsx
+++ mobile/src/VideoPlayer.tsx
@@ -9,12 +9,20 @@ import OptionsMenu from "react-native-option-menu";
 import { useNavigation } from '@react-navigation/native';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import Video from 'react-native-video';
+import { AppSupport, useApp } from './AppSupport';
 
 export function VideoPlayer({uri, done}) {
+  const [mute, setMute] = React.useState(true);
   const [show, setShow] = React.useState(false);
   let timeout = useRef(null);
+  let player = useRef(null);
 
+  let app = useApp();
   useEffect(() => {
+    let config = app.getConfig();
+    if(config != null) {
+      setMute(config.videoMute);
+    }
     return () => {
       if(timeout.current != null) {
         clearTimeout(timeout.current);
@@ -35,7 +43,7 @@ export function VideoPlayer({uri, done}) {
     if(done != null) {
       done();
     }
-  }
+  };
 
   const onShow = () => {
     setShow(true);
@@ -46,22 +54,34 @@ export function VideoPlayer({uri, done}) {
       timeout.current = null;
       setShow(false);
     }, 5000);
-  }
+  };
 
   const onStop = () => {
     if(done != null) {
       done();
     }
-  }
+  };
+
+  const onMute = () => {
+    onShow();
+    setMute(!mute);
+  };
 
   const Controls = () => {
     if(show) {
       return (
-        <TouchableOpacity style={{ position: 'absolute', padding: 16, bottom: 0, width: '100%', alignItems: 'center' }}>
-          <View opacity={0.8}>
-            <Icon name="times-circle-o" style={{ fontSize: 48, color: '#ffffff' }} onPress={onStop} />
-          </View>
-        </TouchableOpacity>
+        <View style={{ position: 'absolute', width: '100%', height: '100%' }}>
+          <TouchableOpacity style={{ position: 'absolute', padding: 16, top: 0, right: 0 }} onPress={onStop} >
+            <View opacity={0.8}>
+              <Icon name="times-circle-o" style={{ fontSize: 32, color: '#ffffff' }} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ position: 'absolute', padding: 16, top: 0, left: 0 }} onPress={onMute}>
+            <View opacity={0.8}>
+              <Icon name={mute ? 'volume-off' : 'volume-up'} style={{ fontSize: 32, color: '#ffffff' }} />
+            </View>
+          </TouchableOpacity>
+        </View>
       );
     }
     return (<></>);
@@ -70,8 +90,8 @@ export function VideoPlayer({uri, done}) {
   return (
     <TouchableOpacity activeOpacity={1}  style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: '#000000', alignItems: 'center', justifyContent: 'center' }} onPress={onShow}>
       <ActivityIndicator animating={true} size="large" color="#ffffff" />
-      <Video source={{uri: uri}} ref={(ref) => { }} onEnd={onEnd} onError={onError} resizeMode="contain"
-          style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, alignSelf: 'center' }} />
+      <Video source={{uri: uri}} ref={(ref) => { player.current = ref }} onEnd={onEnd} onError={onError} resizeMode="contain"
+          muted={mute} style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, alignSelf: 'center' }} />
       <Controls />
     </TouchableOpacity>
   )
